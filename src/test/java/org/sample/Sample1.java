e2b7640c5957fa7d9275578fe4cf2e134596ca71
Udayakumar98 <udaykumar893941@gmail.com>
2022-03-30 20:09:01 +0530

    second push

--- src/test/java/org/sample/Sample1.java
+++ src/test/java/org/sample/Sample1.java
@@ -1,6 +1,8 @@
 package org.sample;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -11,5 +13,9 @@ public class Sample1 {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://adactinhotelapp.com/");
+		WebElement username = driver.findElement(By.id("username"));
+		username.sendKeys("udayakumar");
+		WebElement pass = driver.findElement(By.id("password"));
+		pass.sendKeys("Uk893941@");
 	}
 }
